16b90aea16ca58927a54ebc774c72b91d1acb095
Mikhail Ponomarev <ponomarev45@gmail.com>
2017-09-23 22:03:33 +0300

    Base
    Base config

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack')
+var path = require("path")
+
+const mainDir = path.join(__dirname, '../')
+
+module.exports = {
+  entry: [
+    path.join(mainDir, 'index.js')
+  ],
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.less$/,
+      loaders: ['style-loader', 'css-loader', 'less-loader']
+    }]
+  },
+  output: {
+    path: mainDir + '/dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: mainDir,
+    historyApiFallback: true
+  }
+}

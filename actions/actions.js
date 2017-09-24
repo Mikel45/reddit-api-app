263d954ea76eaef53044b10003386d93c9846ead
Mikhail Ponomarev <ponomarev45@gmail.com>
2017-09-24 16:28:41 +0300

    Last

--- actions/actions.js
+++ actions/actions.js
@@ -1,8 +1,8 @@
 import fetch from 'isomorphic-fetch'
 
-export const REQUEST_POSTS = 'REQUEST_POSTS' 
-export const RECEIVE_POSTS = 'RECEIVE_POSTS', 
-export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT', 
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
 
 export function selectSubreddit(subreddit) {
@@ -19,7 +19,7 @@ export function invalidateSubreddit(subreddit) {
   }
 }
 
-function requestPost(subreddit) {
+function requestPosts(subreddit) {
   return {
     type: REQUEST_POSTS,
     subreddit
@@ -37,9 +37,9 @@ function receivePosts(subreddit, json) {
 
 function fetchPosts(subreddit) {
   return dispatch => {
-    dispatch(requestPost(subreddit))
+    dispatch(requestPosts(subreddit))
     return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-      .then(response = response.json())
+      .then(response => response.json())
       .then(json => dispatch(receivePosts(subreddit, json)))
   }
 }
